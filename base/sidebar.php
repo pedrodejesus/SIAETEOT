5186115e281832913cc257c1a4426390c511238a
Pedro de Jesus <pedro.dejesus1151@gmail.com>
2018-09-06 08:30:34 -0300

    Atualização sidebar e script bd

--- base/sidebar.php
+++ base/sidebar.php
@@ -16,6 +16,12 @@
                                     <li class="nav-item ml-3">
                                         <a href="\projeto/model/remanejamento/remaneja_alu.php" class="nav-link"><i class="far fa-sync-alt"></i> Remanejamento</a>
                                     </li>
+                                    <li class="nav-item ml-3">
+                                        <a href="\projeto/model/ppa/lista_matriculado.php" class="nav-link"><i class="fa fa-clipboard-list"></i> PPA</a>
+                                    </li>
+                                    <li class="nav-item ml-3">
+                                        <a href="\projeto/model/ape/remaneja_alu.php" class="nav-link"><i class="fa fa-clipboard-list"></i> APE</a>
+                                    </li>
                                 </ul>
                             </li>
                             <li class="nav-item">
